08cbb5b9d3ac310039d21e499778342d970fce35
henrysevern <henrysevern2@gmail.com>
2022-04-09 10:36:37 +0000

    Add word bank of difficult words, add animal ASCII art, and link file to run.py

--- word_bank.py
+++ word_bank.py
@@ -1,3 +1,16 @@
-def word_bank():
-    animals = ['dolphin', 'buzzard', 'elephant']
-    words = ['hangman', 'python']
+animals = ['dolphin', 'buzzard', 'elephant']
+
+words = ['affix', 'avenue', 'awkward', 'beekeeper', 'boggle', 'cobweb',
+    'cycle', 'disavow', 'duplex', 'equip', 'exodus', 'funny', 'galaxy', 
+    'gossip', 'icebox', 'injury', 'ivory', 'jackpot', 'jelly',
+    'jockey', 'joking', 'joyful', 'jumbo', 'kayak', 'khaki', 'kiosk',
+    'lengths', 'lucky', 'luxury', 'lymph', 'nightclub', 'onyx', 'ovary',
+    'pajama', 'pneumonia', 'pshaw', 'puppy' 'scratch', 'staff', 'stretch']
+
+
+def animals_text():
+    print(f"              _                 _")
+    print(f"   __ _ _ __ (_)_ __ ___   __ _| |___")
+    print(f"  / _` | '_ \| | '_ ` _ \ / _` | / __|")
+    print(f" | (_| | | | | | | | | | | (_| | \__ \ ")
+    print(f"  \__,_|_| |_|_|_| |_| |_|\__,_|_|___/")
