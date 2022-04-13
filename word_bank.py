25f97862a0f25b5614c74f996f81003f98f45713
henrysevern <henrysevern2@gmail.com>
2022-04-13 13:56:46 +0000

    Add fucntions that print acii art depending on which catergory chosen

--- word_bank.py
+++ word_bank.py
@@ -1,16 +1,54 @@
 animals = ['dolphin', 'buzzard', 'elephant']
 
 words = ['affix', 'avenue', 'awkward', 'beekeeper', 'boggle', 'cobweb',
-    'cycle', 'disavow', 'duplex', 'equip', 'exodus', 'funny', 'galaxy', 
-    'gossip', 'icebox', 'injury', 'ivory', 'jackpot', 'jelly',
-    'jockey', 'joking', 'joyful', 'jumbo', 'kayak', 'khaki', 'kiosk',
-    'lengths', 'lucky', 'luxury', 'lymph', 'nightclub', 'onyx', 'ovary',
-    'pajama', 'pneumonia', 'pshaw', 'puppy' 'scratch', 'staff', 'stretch']
+         'cycle', 'disavow', 'duplex', 'equip', 'exodus', 'funny', 'galaxy',
+         'gossip', 'icebox', 'injury', 'ivory', 'jackpot', 'jelly',
+         'jockey', 'joking', 'joyful', 'jumbo', 'kayak', 'khaki', 'kiosk',
+         'lengths', 'lucky', 'luxury', 'lymph', 'nightclub', 'onyx', 'ovary',
+         'pajama', 'pneumonia', 'pshaw', 'puppy' 'scratch', 'staff', 'stretch']
+
+movies = ['avengers', 'titanic']
+
+cities = ['london', 'sydney']
+
+food = ['banana', 'grapefruit']
 
 
-def animals_text():
+def animals_t():
     print(f"              _                 _")
     print(f"   __ _ _ __ (_)_ __ ___   __ _| |___")
     print(f"  / _` | '_ \| | '_ ` _ \ / _` | / __|")
     print(f" | (_| | | | | | | | | | | (_| | \__ \ ")
     print(f"  \__,_|_| |_|_|_| |_| |_|\__,_|_|___/")
+
+
+def cities_t():
+    print(f"       _ _   _           ")
+    print(f"   ___(_) |_(_) ___  ___ ")
+    print(f"  / __| | __| |/ _ \/ __|")
+    print(f" | (__| | |_| |  __/\__ \ ")
+    print(f"  \___|_|\__|_|\___||___/")
+
+
+def food_t():
+    print(f"  __                 _ ")
+    print(f" / _| ___   ___   __| |")
+    print(f"| |_ / _ \ / _ \ / _` |")
+    print(f"|  _| (_) | (_) | (_| |")
+    print(f"|_|  \___/ \___/ \__,_|")
+
+
+def movies_t():
+    print(f"                      _           ")
+    print(f" _ __ ___   _____   _(_) ___  ___ ")
+    print(f"| '_ ` _ \ / _ \ \ / / |/ _ \/ __|")
+    print(f"| | | | | | (_) \ V /| |  __/\__ \ ")
+    print(f"|_| |_| |_|\___/ \_/ |_|\___||___/")
+
+
+def words_t():                                  
+    print(f"                         ( )     ")
+    print(f" _   _   _   _   _ __   _| | ___ ")
+    print(f"( ) ( ) ( )/ _ \(  __)/ _  |  __)")
+    print(f"| \_/ \_/ | (_) ) |  ( (_| |__  \ ")
+    print(f" \__/\___/ \___/(_)   \__ _)____/ ")
